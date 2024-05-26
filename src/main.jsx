73df23d7efd971bfa6b58577912cdee8c6b73877
saniyaj mallik <saniyajmallik12@gmail.com>
2024-05-26 09:24:09 +0530

    final

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,20 @@ import React from "react";
 
 import ReactDOM from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import './index.css'
+import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId='181727105626-4snohvf7lqrr451oddqpn8uvatdv71me.apps.googleusercontent.com'>
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<GoogleOAuthProvider clientId='181727105626-safpf16kf1on3qrkikevpcbpskdb2cnm.apps.googleusercontent.com'>
 		<Provider store={store}>
 			<App />
 		</Provider>
 	</GoogleOAuthProvider>
-)
+);
 // development use this
-export const base_url = "http://localhost:8080";
-
+// export const base_url = "http://localhost:8080";
 
 // final deploy use this
-// export const base_url = "https://playlister-server.vercel.app";
-
+export const base_url = "https://college-dekho-server.vercel.app";
